332854ffebe7c673a952a1cee831d50698c2aaff
Bakur Pompaev <baavooxa@gmail.com>
2019-03-24 01:25:14 +0300

    first commit

--- src/node.js
+++ src/node.js
@@ -1,18 +1,31 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (this.left == null) {
+            this.left = node;
+            node.parent = this;
+            return;
+        }
+        if (this.right == null) {
+            this.right = node;
+            node.parent = this;
+            return;
+        }
 	}
 
 	removeChild(node) {
-
+		
 	}
 
 	remove() {
-
+		
 	}
 
 	swapWithParent() {
